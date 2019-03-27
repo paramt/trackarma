2110227683dbb71d390b35555d701e5171b95515
Param Thakkar <pht0104@gmail.com>
2019-03-26 21:07:27 -0400

    Dynamically label y-axis
    e.g. Karma (millions) or Karma (thousands)
    
    Other changes:
     - Add line color and width option to constants.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt, matplotlib.dates as mpd
 import datetime
 import constants
 plt.switch_backend('Agg')
+plt.rcParams["figure.figsize"] = (10, 6)
 
 now = datetime.datetime.now()
 
@@ -38,14 +39,23 @@ with open('data/karma-total.txt', 'a') as file:
     file.write(str(karma_total) + "\n")
     karma_totals.append(str(karma_total))
 
+# Format everything
 formatted_dates = [mpd.date2num(datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')) for date in dates]
+karma_totals = [int(x) for x in karma_totals]
+
+if max(karma_totals) > 1000000:
+    karma_totals = [x/1000000 for x in karma_totals]
+    ylabel = "(millions)"
+elif max(karma_totals) > 1000:
+    karma_totals = [x/1000 for x in karma_totals]
+    ylabel = "(thousands)"
 
 # Create chart
 fig, ax = plt.subplots()
 plt.title(constants.chart_title)
 plt.xlabel("Time (UTC)")
-plt.ylabel('Karma')
+plt.ylabel('Karma ' + ylabel)
 fig.autofmt_xdate()
-plt.plot_date(formatted_dates, karma_totals, marker="", linestyle="-")
+plt.plot_date(formatted_dates, karma_totals, marker="", linestyle="-", lw=constants.width, color=constants.color)
 ax.grid(constants.show_grid)
 plt.savefig('data/chart.png')
--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 reddit_name = '[YOUR_REDDIT_USERNAME]'              # [REQUIRED] reddit username
 chart_title = reddit_name + '\'s Karma Over Time'   # [OPTIONAL] chart title
 show_grid = False                                   # [OPTIONAL] chart grids
+color = "#ff5349"                                   # [OPTIONAL] line color
+width = 3                                           # [OPTIONAL] line width
