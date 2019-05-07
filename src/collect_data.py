d97212c9e44417cd69353aac42e2babfdd0846a9
Param Thakkar <pht0104@gmail.com>
2019-05-06 20:41:34 -0400

    Move everything into /src and run Travis from src/test.py

---
+++ src/collect_data.py
@@ -0,0 +1,47 @@
+import os
+import datetime
+import constants
+import praw
+
+
+def main(usePreset: bool, reddit_name=constants.reddit_name):
+    if(usePreset):
+        reddit = praw.Reddit('Bot')
+    else:
+        reddit = praw.Reddit(client_id=os.environ['CLIENT_ID'],
+                             client_secret=os.environ['CLIENT_SECRET'],
+                             user_agent=os.environ['USER_AGENT'])
+
+    redditor = praw.models.Redditor(reddit, name=reddit_name)
+    now = datetime.datetime.now()
+    karma_total = redditor.link_karma + redditor.comment_karma
+
+    # Add datetime to dates.txt
+    with open("data/dates.txt", "r") as file:
+        dates = list(filter(None, file.read().split("\n")))
+
+    dates.append(str(now))
+
+    with open("data/dates.txt", "w") as file:
+        for date in dates:
+            file.write(date + "\n")
+
+    # Add line to karma-delta.txt
+    with open('data/karma-total.txt', 'r') as file:
+        karma_totals = list(filter(None, file.read().split("\n")))
+
+        try:
+            karma_delta = karma_total - int(karma_totals[-1])
+        except IndexError:
+            karma_delta = 0
+
+        with open('data/karma-delta.txt', 'a') as file:
+            file.write(str(karma_delta) + "\n")
+
+    # Add line to karma-total.txt
+    with open('data/karma-total.txt', 'a') as file:
+        file.write(str(karma_total) + "\n")
+        karma_totals.append(str(karma_total))
+
+if __name__ == "__main__":
+    main(True)
